238c332e0dc751844d27179fb4d66f112caa385c
miki <andrasnyarai@gmail.com>
2019-11-10 04:48:20 +0100

    improve base SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ export default function Dashboard() {
   return (
     <>
       <SEO
-        title="Expersonal"
-        keywords={['gatsby', 'react', 'visual', 'experiments', 'css', 'perspective', 'dashboard']}
+        title="expersonal"
+        keywords={['generative art', 'visual experiments', 'javascript', 'andras nyarai', 'fractal']}
       />
       <DashboardScreen />
     </>
