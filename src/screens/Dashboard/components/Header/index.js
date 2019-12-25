b9cd975e0ba30f2ecb3bdc599ea4f7a8394d889a
Andras <andrasnyarai@gmail.com>
2019-12-25 22:14:38 +0100

    debug live theme

--- src/screens/Dashboard/components/Header/index.js
+++ src/screens/Dashboard/components/Header/index.js
@@ -6,6 +6,7 @@ import { HeaderContainer, H1, StyledLink, LogoWrapper } from './style'
 export default function Header({ siteTitle, setTheme, currentTheme }) {
   const [shine, setShine] = useState(currentTheme === 'light')
 
+  console.log(currentTheme, '___theme')
   const switchTheme = () => {
     setTheme(theme => (theme === 'light' ? 'dark' : 'light'))
     setShine(s => !s)
