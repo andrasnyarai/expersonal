80a54ada3303d9c6092517891d205b35c1b12695
Andras <andrasnyarai@gmail.com>
2020-04-04 19:08:31 +0200

    - remove border radius on scroller it caused children to not render properly - remove scroller inset shadow

--- src/screens/LindenmayerSystems/components/SimpleSelector.js
+++ src/screens/LindenmayerSystems/components/SimpleSelector.js
@@ -6,20 +6,8 @@ const Rail = styled.div`
   height: 50px;
   max-width: 80vw;
   ${({ isFloating }) => (isFloating ? '' : 'margin: 0 5px 4px 5px;')};
-  border-radius: 5px;
   align-self: center;
   overflow-x: scroll;
-
-  /* horizontal scrolling shadow */
-  background-image: linear-gradient(to right, white, white), linear-gradient(to right, white, white),
-    linear-gradient(to right, rgba(0, 0, 0, 0.25), rgba(255, 255, 255, 0)),
-    linear-gradient(to left, rgba(0, 0, 0, 0.25), rgba(255, 255, 255, 0));
-
-  background-position: left center, right center, left center, right center;
-  background-repeat: no-repeat;
-  background-color: white;
-  background-size: 20px 100%, 20px 100%, 20px 100%, 20px 100%;
-  background-attachment: local, local, scroll, scroll;
 
   ${({ isVisible }) => (isVisible ? 'opacity:1;z-index:1;' : 'opacity:0;z-index:0;')};
   ${({ isFloating }) => (isFloating ? 'position: absolute;transform: translateY(-21vh);max-width: 75vw;' : '')};
