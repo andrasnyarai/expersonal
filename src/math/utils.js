3ef832a81897d75a0aeaec193e21dec36f2d0f17
Andras <andrasnyarai@gmail.com>
2020-04-16 21:59:19 +0200

    add lerp to utils

--- src/math/utils.js
+++ src/math/utils.js
@@ -2,4 +2,8 @@ function map(value, [minFrom, maxFrom], [minTo, maxTo]) {
   return ((value - minFrom) * (maxTo - minTo)) / (maxFrom - minFrom) + minTo
 }
 
-export { map }
+function lerp(alpha, min, max) {
+  return min * (1 - alpha) + max * alpha
+}
+
+export { map, lerp }
