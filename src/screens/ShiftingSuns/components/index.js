d54484ac6b5bbbde553697faaa5c0f413d97dab5
rush <andrasnyarai@gmail.com>
2021-07-01 20:16:43 +0200

    remove old page

--- src/screens/ShiftingSuns/components/index.js
+++
@@ -1,7 +0,0 @@
-import OuterContainer from './OuterContainer'
-import InnerContainer from './InnerContainer'
-import Circle from './Circle'
-import BlobContainer from './BlobContainer'
-import Blob from './Blob'
-
-export { OuterContainer, InnerContainer, Circle, BlobContainer, Blob }
