266f25d63cc0bd3d9cda483002b849c0d26cc918
Andras Nyarai <andrasnyarai@gmail.com>
2021-09-25 17:21:20 +0200

    update manifest background color

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,8 +47,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#000000',
+        theme_color: '#000000',
         display: 'minimal-ui',
         icon: 'src/images/expr.png', // This path is relative to the root of the site.
       },
